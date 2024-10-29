12bddeeed2100702a446c19168a717ff96ec5182
Kacper-Grzyb <kgrzyb2860@gmail.com>
2024-10-29 21:44:59 +0100

    navbar finished without animation

--- BrickedUp/resources/views/settings.blade.php
+++ BrickedUp/resources/views/settings.blade.php
@@ -6,8 +6,149 @@
     <title>Settings</title>
 </head>
 <body>
+    <div class = "nav-header">
+        <ul class="navbar">
+            <div> {{--This is in a div so that the allignment is correct--}}
+                <li>
+                    <img src="{{asset('images/home_icon.svg')}}" alt="home icon">
+                </li>
+                <li>
+                    <img src="{{asset('images/chart_icon.svg')}}" alt="chart icon">
+                </li>
+                <li>
+                    <img src="{{asset('images/settings_icon_highlighted.svg')}}" alt="settings icon">
+                </li>
+                <li>
+                    <img src="{{asset('images/user_icon.svg')}}" alt="user icon">
+                </li>
+            </div>
+            <li>
+                <div class="searchbar">
+                    <button>
+                        <img src="{{asset('images/search_icon.svg')}}" alt="search icon">
+                    </button>
+                    <input type="text" placeholder="Search for set...">
+                </div>
+            </li>
+        </ul>
     
-    {{-- Navbar goes here --}}
+        <ul class="set-prices-sidescroller">
+            <li class="sidescroller-box">
+                <div class="sidescroller-box-status-green"></div>
+                <div class="sidescroller-box-content">
+                    <img src="{{asset('images/lego_logo.svg')}}" alt="lego logo">
+                    <h6>76453</h6>
+                    <p>+1.37%</p>
+                </div>
+            </li>
+            <li class="sidescroller-box">
+                <div class="sidescroller-box-status-green"></div>
+                <div class="sidescroller-box-content">
+                    <img src="{{asset('images/lego_logo.svg')}}" alt="lego logo">
+                    <h6>76453</h6>
+                    <p>+1.37%</p>
+                </div>
+            </li>
+            <li class="sidescroller-box">
+                <div class="sidescroller-box-status-green"></div>
+                <div class="sidescroller-box-content">
+                    <img src="{{asset('images/lego_logo.svg')}}" alt="lego logo">
+                    <h6>76453</h6>
+                    <p>+1.37%</p>
+                </div>
+            </li>
+            <li class="sidescroller-box">
+                <div class="sidescroller-box-status-green"></div>
+                <div class="sidescroller-box-content">
+                    <img src="{{asset('images/lego_logo.svg')}}" alt="lego logo">
+                    <h6>76453</h6>
+                    <p>+1.37%</p>
+                </div>
+            </li>
+            <li class="sidescroller-box">
+                <div class="sidescroller-box-status-green"></div>
+                <div class="sidescroller-box-content">
+                    <img src="{{asset('images/lego_logo.svg')}}" alt="lego logo">
+                    <h6>76453</h6>
+                    <p>+1.37%</p>
+                </div>
+            </li>
+            <li class="sidescroller-box">
+                <div class="sidescroller-box-status-green"></div>
+                <div class="sidescroller-box-content">
+                    <img src="{{asset('images/lego_logo.svg')}}" alt="lego logo">
+                    <h6>76453</h6>
+                    <p>+1.37%</p>
+                </div>
+            </li>
+            <li class="sidescroller-box">
+                <div class="sidescroller-box-status-green"></div>
+                <div class="sidescroller-box-content">
+                    <img src="{{asset('images/lego_logo.svg')}}" alt="lego logo">
+                    <h6>76453</h6>
+                    <p>+1.37%</p>
+                </div>
+            </li>
+            <li class="sidescroller-box">
+                <div class="sidescroller-box-status-green"></div>
+                <div class="sidescroller-box-content">
+                    <img src="{{asset('images/lego_logo.svg')}}" alt="lego logo">
+                    <h6>76453</h6>
+                    <p>+1.37%</p>
+                </div>
+            </li>
+            <li class="sidescroller-box">
+                <div class="sidescroller-box-status-green"></div>
+                <div class="sidescroller-box-content">
+                    <img src="{{asset('images/lego_logo.svg')}}" alt="lego logo">
+                    <h6>76453</h6>
+                    <p>+1.37%</p>
+                </div>
+            </li>
+
+            <li class="sidescroller-box">
+                <div class="sidescroller-box-status-green"></div>
+                <div class="sidescroller-box-content">
+                    <img src="{{asset('images/lego_logo.svg')}}" alt="lego logo">
+                    <h6>76453</h6>
+                    <p>+1.37%</p>
+                </div>
+            </li>
+            <li class="sidescroller-box">
+                <div class="sidescroller-box-status-green"></div>
+                <div class="sidescroller-box-content">
+                    <img src="{{asset('images/lego_logo.svg')}}" alt="lego logo">
+                    <h6>76453</h6>
+                    <p>+1.37%</p>
+                </div>
+            </li>
+            <li class="sidescroller-box">
+                <div class="sidescroller-box-status-green"></div>
+                <div class="sidescroller-box-content">
+                    <img src="{{asset('images/lego_logo.svg')}}" alt="lego logo">
+                    <h6>76453</h6>
+                    <p>+1.37%</p>
+                </div>
+            </li>
+            <li class="sidescroller-box">
+                <div class="sidescroller-box-status-green"></div>
+                <div class="sidescroller-box-content">
+                    <img src="{{asset('images/lego_logo.svg')}}" alt="lego logo">
+                    <h6>76453</h6>
+                    <p>+1.37%</p>
+                </div>
+            </li>
+            <li class="sidescroller-box">
+                <div class="sidescroller-box-status-green"></div>
+                <div class="sidescroller-box-content">
+                    <img src="{{asset('images/lego_logo.svg')}}" alt="lego logo">
+                    <h6>76453</h6>
+                    <p>+1.37%</p>
+                </div>
+            </li>
+            
+        </ul>
+    </div>
 
     <h1>Settings</h1>
 
