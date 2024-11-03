78ab71138dc3db486a8ca1ae5eeeb91824305816
Seb <sedea23@student.sdu.dk>
2024-11-03 12:03:58 +0100

    Home page done, except chart

--- BrickedUp/resources/views/home.blade.php
+++ BrickedUp/resources/views/home.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Home</title>
+    <title>BrickedUp - Home</title>
 </head>
 <body>
 <body>
@@ -11,13 +11,13 @@
         <ul class="navbar">
             <div> {{--This is in a div so that the allignment is correct--}}
                 <li>
-                    <img src="{{asset('images/home_icon.svg')}}" alt="home icon">
+                    <img src="{{asset('images/home_icon_highlighted.svg')}}" alt="home icon">
                 </li>
                 <li>
                     <img src="{{asset('images/chart_icon.svg')}}" alt="chart icon">
                 </li>
                 <li>
-                    <img src="{{asset('images/settings_icon_highlighted.svg')}}" alt="settings icon">
+                    <img src="{{asset('images/settings_icon.svg')}}" alt="settings icon">
                 </li>
                 <li>
                     <img src="{{asset('images/user_icon.svg')}}" alt="user icon">
@@ -151,44 +151,105 @@
         </ul>
     </div>
 
-    <div class="terminal-box ">
-        <div class="top-ranked-sets-text">
-            <h2>Top Ranked Sets</h2>
-
-            <div class="set-details-list">
-                <div class="set-details-row">
-                    <h3>Set Number</h3>
-                    <p>10123</p>
-                </div>
-                <div class="set-details-row">
-                    <h3>Name</h3>
-                    <p>Cloud City</p>
-                </div>
-                <div class="set-details-row">
-                    <h3>Theme</h3>
-                    <a href="kawaszlugbombakupa">Star Wars</a>
-                </div>
-                <div class="set-details-row">
-                    <h3>Subtheme</h3>
-                    <a href="kawaszlugbombakupa">Episode V</a>
-                </div>
-                <div class="set-details-row">
-                    <h3>Year</h3>
-                    <a href="kawaszlugbombakupa">2003</a>
-                </div>
-                <div class="set-details-row">
-                    <h3>Availability</h3>
-                    <a href="kawaszlugbombakupa">Retired</a>
-                </div>
-                <div class="set-details-row">
-                    <h3>Pieces</h3>
-                    <p>698</p>
-                </div>
-                <div class="set-details-row">
-                    <h3>Minifigures</h3>
-                    <p>7</p>
-                </div>
+    <div class="home-top-ranked-container" style="align-items: start">
+        
+        <div class="home-top-ranked-subcontainer">
+            
+            <div class="terminal-box home-chart">
+                <h2>Set Value Chart</h2>
+                <p>Insert Chart here</p>
             </div>
+            <div class="terminal-box container">
+                <table>
+                    <tr>
+                        <th>Top Ranked News </th>
+                        <th class="time">Type</th>
+                        <th class="time">Time</th>
+                    </tr>
+                    <tr class="row">
+                        <td class="news-title">LEGO Reports Stable Financial Results for H1 2023</td>
+                        <td class="type">LN</td>
+                        <td class="time">13:00</td>
+                    </tr>
+                    <tr class="row">
+                        <td class="news-title">LEGO Pauses Recycled PET Bricks Initiative</td>
+                        <td class="type">LN</td>
+                        <td class="time">12:45</td>
+                    </tr>
+                    <tr class="row">
+                        <td class="news-title">LEGO Expands into Emerging Markets with 50 New Stores</td>
+                        <td class="type">LN</td>
+                        <td class="time">11:30</td>
+                    </tr>
+                    <tr class="row">
+                        <td class="news-title">LEGO Education Launches New SPIKE Robotics Kit</td>
+                        <td class="type">LN</td>
+                        <td class="time">10:50</td>
+                    </tr>
+                    <tr class="row">
+                        <td class="news-title">LEGO-Disney Collaboration for 100th Anniversary Unveiled</td>
+                        <td class="type">LN</td>
+                        <td class="time">10:15</td>
+                    </tr>
+                    <tr class="row">
+                        <td class="news-title">LEGO Reveals New AI-Powered Build Assist Tool</td>
+                        <td class="type">LN</td>
+                        <td class="time">09:55</td>
+                    </tr>
+                    <tr class="row">
+                        <td class="news-title">LEGO Ninjago Movie Sequel Confirmed for 2024 Release</td>
+                        <td class="type">LN</td>
+                        <td class="time">09:30</td>
+                    </tr>
+                    <tr class="row">
+                        <td class="news-title">LEGO Launches Eco-Friendly Packaging Initiative Globally</td>
+                        <td class="type">LN</td>
+                        <td class="time">09:10</td>
+                    </tr>
+                    <tr class="row">
+                        <td class="news-title">LEGO Sees Surge in Adult Fans with New 'LEGO Icons' Series</td>
+                        <td class="type">LN</td>
+                        <td class="time">08:45</td>
+                    </tr>
+                </table>
+            </div>
+            
         </div>
+        <div class="terminal-box home-top-ranked-text">
+            <h2>Lego Set &emsp; Price &emsp; Change &emsp; Index</h2>
+            
+            <div class="top-set">
+                <p class="top-set-name">Star Destroyer</p>
+                <p class="for-sale-price">$139,99</p>
+                <p class="positive-change">+0.42</p>
+                <p class="for-sale-price">1.2103</p>
+            </div>
+            <div class="top-set">
+                <p class="top-set-name">Hogwarts Castle</p>
+                <p class="for-sale-price">$129.99</p>
+                <p class="negative-change">-0.18</p>
+                <p class="for-sale-price">1.8703</p>
+            </div>
+            <div class="top-set">
+                <p class="top-set-name">Technic Bugatti</p>
+                <p class="for-sale-price">$349.99</p>
+                <p class="positive">+0.31</p>
+                <p class="for-sale-price">1.8933</p>
+            </div>
+            <div class="top-set">
+                <p class="top-set-name">Millennium Falcon</p>
+                <p class="for-sale-price">$799.99</p>
+                <p class="positive-change">+0.50</p>
+                <p class="for-sale-price">1.1671</p>
+            </div>
+            <div class="top-set">
+                <p class="top-set-name">City Police HQ</p>
+                <p class="for-sale-price">$99,99</p>
+                <p class="negative-change">-0.26</p>
+                <p class="for-sale-price">1.0108</p>
+            </div>
+
+        </div>
+    </div>
 </body>
 </html>
