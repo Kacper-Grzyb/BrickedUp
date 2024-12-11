9904ba5a0c487845764291d836b66a935fd3b28c
Seb <sedea23@student.sdu.dk>
2024-12-11 16:24:02 +0100

    cosmetic changes

--- BrickedUp/resources/views/profile/profile.blade.php
+++ BrickedUp/resources/views/profile/profile.blade.php
@@ -4,18 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
     <title>{{auth()->user()->name}} Profile</title>
 </head>
 <body>
     <x-navbar :currentPage='"profile"'/>
 
-    <img src="{{asset('img/chuj.webp')}}" alt="">
     <h1>Welcome {{auth()->user()->name}}</h1>
-    <div class="terminal-box">
-        User Inventory
-    </div>
-    <div class="terminal-box">
-        User Reviews
+    <div class="profile-container">
+        <div class="terminal-box profile-left-box">
+            <img src="{{asset('img/chuj.webp')}}" width="200" height="200" style="border-radius: 3px;" alt="profile-picture">
+            <div class="profile-username">
+                Username: {{auth()->user()->name}}
+            </div>
+
+        </div>
+
+        <div class="profile-subcontainer">
+
+            
+            <div class="terminal-box">
+                User Inventory <br/>
+                User Inventory
+            </div>
+            <div class="terminal-box">
+                User Reviews
+            </div>
+        </div>
     </div>
 </body>
 </html>
