8ceba5be4d5207c249bd92c9f898552d8b21f247
Leonardo <legia23@student.sdu.dk>
2024-12-15 14:26:14 +0100

    updated version with the notifications

---
+++ BrickedUp/app/Http/Controllers/PriceAlertController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controller;
+
+use Illuminate\Http\Request;
+use App\Models\PriceAlert;
+use Illuminate\Support\Facades\Auth;
+
+class PriceAlertController extends Controller
+{
+    public function store(Request $request)
+    {
+
+        $validated = $request->validate([
+            'setName' => 'required|string|max:255',
+            'targetPrice' => 'required|numeric|min:0',
+        ]);
+
+        $priceAlert = PriceAlert::create([
+            'user_id' => Auth()::id(),
+            'set_name' => $validated['setName'],
+            'target_price' => $validated['targetPrice'],
+        ]);
+
+        if ($priceAlert){
+            return response()->json([
+                'success' => true,
+                'message' => 'Price alert has been set successfully.'
+            ]);
+        }
+        else{
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to set price alert.'
+            ], 500);
+        }
+    }
+}
