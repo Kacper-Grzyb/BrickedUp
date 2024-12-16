a5be255f89ce64afc0bcef03ba886edc5bdbd422
frske <fsche23@student.sdu.dk>
2024-12-16 21:14:56 +0000

    fixed edit profile

--- BrickedUp/resources/views/profile/edit-profile.blade.php
+++ BrickedUp/resources/views/profile/edit-profile.blade.php
@@ -1,35 +1,37 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Update Profile Information</title>
 </head>
-<body>
-    
-    @include('components.navbar', ['currentPage' => 'settings'])
 
+<body>
+
+
+    <x-navbar :currentPage='"settings"' />
     <h1>Update Profile Information</h1>
 
     @if ($errors->updatePassword->any())
-        <div id="error-message" class="alert-message terminal-box error">
-            @foreach($errors->updatePassword->all() as $error)
-                {{ $error }}
-            @endforeach
-        </div>
+    <div id="error-message" class="alert-message terminal-box error">
+        @foreach($errors->updatePassword->all() as $error)
+        {{ $error }}
+        @endforeach
+    </div>
 
-        <script>
-            document.addEventListener('DOMContentLoaded', function() {
-                setTimeout(function () {
-                    let message = document.getElementById('error-message');
-                    if(message) {
-                        message.style.transition = 'opacity 0.5s ease';
-                        message.style.opacity = '0'
-                        setTimeout(() => message.remove(), 500)
-                    }
-                }, 5000)
-            })
-        </script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            setTimeout(function() {
+                let message = document.getElementById('error-message');
+                if (message) {
+                    message.style.transition = 'opacity 0.5s ease';
+                    message.style.opacity = '0'
+                    setTimeout(() => message.remove(), 500)
+                }
+            }, 5000)
+        })
+    </script>
     @endif
 
     <div class="settings-container">
@@ -47,12 +49,14 @@
                 <h3>New Email: </h3>
                 <input type="email" name="email" id="email" value="{{auth()->user()->email}}">
             </div>
-            <button type="submit"><p class="fake-link">Update Username and Email</p></button>
+            <button type="submit">
+                <p class="fake-link">Update Username and Email</p>
+            </button>
         </form>
         <form class="terminal-box-edit" method="post" action="{{route('password.update')}}">
             @csrf
             @method('put')
-            
+
             <div class="settings-row">
                 <h3>Current Password: </h3>
                 <input type="password" name="current_password" id="update_password_current_password" placeholder="Current Password">
@@ -65,10 +69,13 @@
                 <h3>Confirm Password:</h3>
                 <input type="password" name="password_confirmation" id="update_password_password_confirmation" placeholder="Confirm Password">
             </div>
-            <button type="submit"><p class="fake-link">Update Password</p></button>
+            <button type="submit">
+                <p class="fake-link">Update Password</p>
+            </button>
         </form>
 
         <a style="margin-top: 2rem" href="/settings">Go back to settings</a>
     </div>
 </body>
+
 </html>
